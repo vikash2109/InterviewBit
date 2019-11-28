a0d2b41c1d76882f830c1be468196ac96bc5ceb2
vikash2109 <vikash.nitmz@gmail.com>
2019-11-28 18:03:38 +0530

    Largest BST subtree from Binary Tree

--- IB_Academy/src/LargestBstSubTree.java
+++ IB_Academy/src/LargestBstSubTree.java
@@ -1,4 +1,33 @@
- class TreeNode {
+/*
+ Largest BST Subtree
+Given a binary tree. Find the size of largest subtree which is a Binary Search Tree (BST), where largest means subtree with the largest number of nodes in it. Note: A subtree must include all of its descendants. For Example
+Input 1:
+                     10
+                    / \
+                   5  15
+                  / \   \ 
+                 1   8   7
+Output 1:
+    3
+    Explanation 1:
+        Largest BST subtree is
+                            5
+                           / \
+                          1   8
+
+Input 2:
+                5
+               / \
+              3   8
+             / \ / \
+            1  4 7  9
+Output 2:
+     7
+     Explanation 2:
+        Given binary tree itself is BST.
+ */
+
+class TreeNode {
      int val;
      TreeNode left;
      TreeNode right;
